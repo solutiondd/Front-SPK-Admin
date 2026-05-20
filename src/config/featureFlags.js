667b829c8868be82d42a6e0046883c80e69d1262
Thitipong <rattiya1743@gmail.com>
2026-05-20 15:57:16 +0700

    Update 20/05/2569

--- src/config/featureFlags.js
+++ src/config/featureFlags.js
@@ -1,23 +1,20 @@
-// เก็บการตั้งค่าเปิด/ปิดฟีเจอร์สำหรับแต่ละโรงเรียน
-
 export default {
+  
   // ฟีเจอร์สำหรับตารางเข้า-ออก
   attendance: {
-    enableLineupColumn: import.meta.env.VITE_ENABLE_LINEUP_COLUMN === "false",
+    enableLineupColumn: false, // true = แสดงคอลัมน์เข้าแถว, false = ซ่อน
   },
 
   // ฟีเจอร์สำหรับหน้าเช็คชื่อ
   checkName: {
     // person_confirmation: ต้องมี usecase = person_confirmation
     // any_timestamp: มี timeStamps มากกว่า 0 ก็ถือว่ามา
-    presentMode:
-      import.meta.env.VITE_CHECKNAME_PRESENT_MODE || "any_timestamp",
+    presentMode: "any_timestamp", // หมดการพิจารณาว่ามาแล้วหรือยังสำหรับหน้าเช็คชื่อ
   },
 
   // ฟีเจอร์สำหรับอุปกรณ์
   device: {
-    enableUseCase: import.meta.env.VITE_ENABLE_DEVICE_USECASE === "false",
+    enableUseCase: true, // true = แสดง Use Case, false = ซ่อน
   },
 
-  // ฟีเจอร์อื่น ๆ (เพิ่มได้ตามต้องการ)
 };
